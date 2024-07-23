652995629a5438c5c952f212b46ca924ccf854a6
10Soa <soaornella5@gmail.com>
2024-07-23 21:26:41 +0300

    Initial commit

---
+++ src/test/java/com/example/takalobazaarspringboot/TakaloBazaarSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.takalobazaarspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakaloBazaarSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
