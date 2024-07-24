a3cd9b8c80f114c10052a88eac5f91b7ad96c803
Loïc <lorandrianarivelo@gmail.com>
2024-07-24 21:49:59 +0300

    Init template

---
+++ src/test/java/com/farhan/staradmin/SpringBootThymeleafStarAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.takalobazar.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringBootThymeleafStarAdminApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
